9e5968a0d14883eec6d35c5c3575f1e9ebc58f4c
William Giles <william@williamgiles.co.nz>
2025-12-15 22:47:29 +1300

    Enhance MobileNav component by adding PanelLeftIcon for improved visual appeal and updating button styles for better user interaction. Adjust layout structure for a cleaner design.

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "~/components/ui/button";
+import { PanelLeftIcon } from "lucide-react";
 
 interface MobileNavProps {
   onMenuToggle: () => void;
@@ -9,16 +10,19 @@ interface MobileNavProps {
 export function MobileNav({ onMenuToggle }: MobileNavProps) {
   return (
     <nav className="sticky top-0 left-0 right-0 w-full bg-white/75 backdrop-blur dark:bg-black/50 h-16 z-50 border-b border-black/10 dark:border-white/10">
-      <div className="flex items-center justify-between">
-        <div className="absolute flex items-center justify-center top-0 left-2 h-16 w-16 z-30">
-          <Button 
-            variant="link" 
-            className="text-lg uppercase text-black dark:text-white" 
-            onClick={onMenuToggle}
-          >
+      <div className="flex items-center justify-between h-full px-4">
+        {/* <div className="absolute flex items-center justify-center top-0 left-2 h-16 w-16 z-30"> */}
+        <Button
+          variant="ghost"
+          className="text-lg uppercase  text-black dark:text-white group"
+          onClick={onMenuToggle}
+        >
+          <div className="flex items-center gap-2 group-hover:border-b-2 group-hover:border-black dark:group-hover:border-white transition-colors">
+            <PanelLeftIcon />
             Menu
-          </Button>
-        </div>
+          </div>
+        </Button>
+        {/* </div> */}
       </div>
     </nav>
   );
